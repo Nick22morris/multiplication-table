e02b1ad526e6267bb74c241e0b6c561b2b6eda21
Nick <65043773+Nick22morris@users.noreply.github.com>
2022-01-24 23:02:52 -0600

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,80 @@
+
+public class MultiplicationTable {
+	private int grid[][];
+	
+	public MultiplicationTable() {
+		grid = new int[10][10];
+		create();
+	}
+	public  MultiplicationTable(int rows, int columns) {
+		grid = new int[rows][columns];
+		create();
+	}
+	public void create() {
+		int y = 1;
+		int x = 0;
+		// Sets top axis to 1...10
+		for(int n : grid[0]) {
+			grid[0][x] =  x+1; 
+			x++;
+		}
+		
+		// Sets left axis to 1...10
+		for(int arr[] : grid) {
+			arr[0] = y;
+			y++;
+		}
+		
+		//Prints array
+//		for(int arr[] : grid) {
+//			for( int n : arr)
+//				System.out.print(n + " ");
+//			System.out.println();
+//		}
+//		System.out.println();
+		
+		// Creates String Matrix]
+		for(int j = 0; j < grid.length; j++) {
+			for(int i = 0; i < grid[j].length; i++) {
+				int top = grid[0][j];
+				int left = grid[i][0];
+				int product = top * left;
+				grid[i][j] = product;
+				x++;
+			}
+		}
+		
+		//Prints array
+//		for(int arr[] : grid) {
+//			for( int n : arr)
+//				System.out.print(n + " ");
+//			System.out.println();
+//		}
+		
+		// Gets Biggest Value digits
+		int yLen = grid.length - 1;
+		int xLen = grid[yLen].length - 1;		
+		int biggest  = grid[yLen][xLen];
+		String biggestString = biggest + "";
+		String newMatrix[][]= new String[yLen+1][xLen+1];
+		// Cleans it up
+		for(int j = 0; j < grid.length; j++) {
+			for(int i = 0; i < grid[j].length; i++) {
+				int val = grid[j][i];
+				String sVal = val + "";
+				int len = 5 - sVal.length();
+				int added = 0;
+				while(added < len) {
+					sVal += " ";
+					added++;
+				}
+				newMatrix[j][i] = sVal;
+				System.out.print(sVal + "  ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+	
+}
---
+++ runner.java
@@ -0,0 +1,9 @@
+
+
+public class runner {
+
+	public static void main(String[] args) {
+		MultiplicationTable mt = new MultiplicationTable(5,5);
+	}
+
+}
